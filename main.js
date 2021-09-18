e140bfd34b64a8626fc07d877663c98d8fffb046
Nate Teeples <54116891+45471158665775254@users.noreply.github.com>
2021-09-18 12:29:24 -0600

    Edit

--- main.js
+++ main.js
@@ -1,15 +1,16 @@
 const firebaseConfig = {
     apiKey: "AIzaSyDIX99_rLlyHVluPDLljY6C874UZzV9-i8",
     authDomain: "name-chain.firebaseapp.com",
+    databaseURL: "https://name-chain-default-rtdb.firebaseio.com",
     projectId: "name-chain",
     storageBucket: "name-chain.appspot.com",
     messagingSenderId: "1052953908354",
     appId: "1:1052953908354:web:0bc7873af4c5e43d36d09b",
     measurementId: "G-GV3QL2ZSE1"
-};
+  };
   
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
+var app = initializeApp(firebaseConfig);
 
 // Main Code ------------------------------------------------------------------------------------------
 var nameV;
